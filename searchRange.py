9fa3be1e9c58911facd6128c1ef2f2a12468174d
Hcyand <897488242@qq.com>
2019-08-08 19:32:32 +0800

    searchRange

---
+++ searchRange.py
@@ -0,0 +1,57 @@
+"""
+给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+
+你的算法时间复杂度必须是 O(log n) 级别。
+
+如果数组中不存在目标值，返回 [-1, -1]。
+
+示例 1:
+
+输入: nums = [5,7,7,8,8,10], target = 8
+输出: [3,4]
+
+示例 2:
+
+输入: nums = [5,7,7,8,8,10], target = 6
+输出: [-1,-1]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+import unittest
+
+
+# O(log n)时间要求
+# 方法：双指针，二分法
+# 先找最左标，再找最右标
+class Solution:
+    def extreme_insertion_index(self, nums, target, left):
+        lo = 0
+        hi = len(nums)
+
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if nums[mid] > target or (left and target == nums[mid]):
+                hi = mid
+            else:
+                lo = mid + 1
+
+        return lo
+
+    def searchRange(self, nums, target):
+        left_idx = self.extreme_insertion_index(nums, target, True)
+
+        # assert that `left_idx` is within the array bounds and that `target`
+        # is actually in `nums`.
+        if left_idx == len(nums) or nums[left_idx] != target:
+            return [-1, -1]
+
+        return [left_idx, self.extreme_insertion_index(nums, target, False) - 1]
+
+
+class TestSearchRange(unittest.TestCase):
+    def test_init(self):
+        d = Solution()
+        self.assertEqual(d.searchRange([2, 2, 2, 2], 2), [0, 3])
+        self.assertEqual(d.searchRange([], 2), [-1, -1])

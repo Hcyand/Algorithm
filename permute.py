1bab3dda7c9f47c65603454f449ed67f3b54b7c5
Hcyand <897488242@qq.com>
2019-08-11 10:40:45 +0800

    permute 回溯法

--- permute.py
+++ permute.py
@@ -18,13 +18,14 @@
 链接：https://leetcode-cn.com/problems/permutations
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
+
+
 # 回溯算法关键在于:不合适就退回上一步
 # 然后通过约束条件, 减少时间复杂度.
-from typing import List
 
 
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
+    def permute(self, nums: [int]) -> [[int]]:
         res = []
 
         def backtrack(nums, tmp):

bf726916fb67efdc7c30ab346c89eb931f5d4b40
Hcyand <897488242@qq.com>
2019-08-11 10:20:40 +0800

    permute 回溯法

---
+++ permute.py
@@ -0,0 +1,38 @@
+"""
+给定一个没有重复数字的序列，返回其所有可能的全排列。
+
+示例:
+
+输入: [1,2,3]
+输出:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/permutations
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+# 回溯算法关键在于:不合适就退回上一步
+# 然后通过约束条件, 减少时间复杂度.
+from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtrack(nums, tmp):
+            if not nums:
+                res.append(tmp)
+                return
+            for i in range(len(nums)):
+                backtrack(nums[:i] + nums[i + 1:], tmp + [nums[i]])
+
+        backtrack(nums, [])
+        return res

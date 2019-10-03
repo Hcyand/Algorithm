9e6b58498c74269598ea68f899de22dba50324ef
Hcyand <897488242@qq.com>
2019-10-03 11:07:09 +0800

    num trees 2

---
+++ numTrees2.py
@@ -0,0 +1,47 @@
+"""
+给定一个整数 n，生成所有由 1 ... n 为节点所组成的二叉搜索树。
+
+示例:
+
+输入: 3
+输出:
+[
+  [1,null,3,2],
+  [3,2,null,1],
+  [3,1,null,null,2],
+  [2,1,3],
+  [1,null,2,null,3]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/unique-binary-search-trees-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def generateTrees(self, n: int) -> [TreeNode]:
+        def generate_trees(start, end):
+            if start > end:
+                return [None, ]
+            all_trees = []
+            for i in range(start, end + 1):
+                left_trees = generate_trees(start, i - 1)
+                right_trees = generate_trees(i + 1, end)
+                for l in left_trees:
+                    for r in right_trees:
+                        current_tree = TreeNode(i)
+                        current_tree.left = l
+                        current_tree.right = r
+                        all_trees.append(current_tree)
+            return all_trees
+
+        return generate_trees(1, n) if n else []

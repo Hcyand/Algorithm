aad8ba79fc62242e31c5ca4845b4cf1f861e1de9
Hcyand <897488242@qq.com>
2019-10-04 10:38:07 +0800

    num trees 2

---
+++ isValidBST.py
@@ -0,0 +1,55 @@
+"""
+给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+
+假设一个二叉搜索树具有如下特征：
+
+
+节点的左子树只包含小于当前节点的数。
+节点的右子树只包含大于当前节点的数。
+所有左子树和右子树自身必须也是二叉搜索树。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/validate-binary-search-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    # 递归
+    def isValidBST(self, root: TreeNode) -> bool:
+        def helper(node, lower=float("-inf"), upper=float("inf")):
+            if not node:
+                return True
+            val = node.val
+            if val <= lower or val >= upper:
+                return False
+            if not helper(node.right, val, upper):
+                return False
+            if not helper(node.left, lower, val):
+                return False
+            return True
+
+        return helper(root)
+
+    # 迭代
+    # 迭代一般需要用到栈辅助空间
+    def isValidBST2(self, root: TreeNode) -> bool:
+        stack = [(root, float("-inf"), float("inf"))]
+        while stack:
+            root, lower, upper = stack.pop()
+            if not root:
+                continue
+            val = root.val
+            if val <= lower or val >= upper:
+                return False
+            stack.append((root.right, val, upper))
+            stack.append((root.left, lower, val))
+        return True

46d09012eaf3175a51a6e73ac4ab65a7fb08a358
Hcyand <897488242@qq.com>
2019-10-14 10:49:49 +0800

    ladder length

--- ladderLength.py
+++ ladderLength.py
@@ -2,18 +2,18 @@
 给定两个单词（beginWord 和 endWord）和一个字典，找到从 beginWord 到 endWord 的最短转换序列的长度。转换需遵循如下规则：
 
 
-	每次转换只能改变一个字母。
-	转换过程中的中间单词必须是字典中的单词。
+每次转换只能改变一个字母。
+转换过程中的中间单词必须是字典中的单词。
 
 
 说明:
 
 
-	如果不存在这样的转换序列，返回 0。
-	所有单词具有相同的长度。
-	所有单词只由小写字母组成。
-	字典中不存在重复的单词。
-	你可以假设 beginWord 和 endWord 是非空的，且二者不相同。
+如果不存在这样的转换序列，返回 0。
+所有单词具有相同的长度。
+所有单词只由小写字母组成。
+字典中不存在重复的单词。
+你可以假设 beginWord 和 endWord 是非空的，且二者不相同。
 
 
 示例 1:
@@ -45,7 +45,40 @@ wordList = ["hot","dot","dog","lot","log"]
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
 
+from collections import defaultdict
 
+
+# 判断两个单词是否只差一个数字，无向无权图，指向公告的结点
 class Solution:
+    def __init__(self):
+        self.length = 0
+        self.all_combo_dict = defaultdict(list)
+
+    def visitWordNode(self, queue, visited, others_visited):
+        current_word, level = queue.pop()
+        for i in range(self.length):
+            intermediate_word = current_word[:i] + "*" + current_word[i + 1:]
+            for word in self.all_combo_dict[intermediate_word]:
+                if word in others_visited:
+                    return level + others_visited[word]
+                if word not in visited:
+                    visited[word] = level + 1
+                    queue.append((word, level + 1))
+        return None
+
     def ladderLength(self, beginWord, endWord, wordList: [str]) -> int:
-        n = 0
+        if endWord not in wordList or not endWord or not beginWord or not wordList:
+            return 0
+        self.length = len(beginWord)
+        for word in wordList:
+            for i in range(self.length):
+                self.all_combo_dict[word[:i] + "*" + word[i + 1:]].append(word)
+        queue_begin = [(beginWord, 1)]
+        queue_end = [(endWord, 1)]
+        visited_begin = {beginWord: 1}
+        visited_end = {endWord: 1}
+        ans = None
+        while queue_begin and queue_end:
+            ans = self.visitWordNode(queue_begin, visited_begin, visited_end)
+            if ans:
+                return ans

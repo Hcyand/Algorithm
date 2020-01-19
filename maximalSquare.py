55c2fb29a8313817bf4a1ced3e0e9f1d91cde298
Hcyand <897488242@qq.com>
2020-01-19 14:21:56 +0800

    最大正方形（动态规划方法）

---
+++ maximalSquare.py
@@ -0,0 +1,36 @@
+"""
+在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+
+示例:
+
+输入:
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+
+输出: 4
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximal-square
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+# 动态规划方法
+# 利用同时满足外围一圈的条件，转换为min(dp[i-1][j]...) + 1,很巧妙
+class Solution:
+    def maximalSquare(self, matrix: [[str]]) -> int:
+        if not matrix:
+            return 0
+        row = len(matrix)
+        col = len(matrix[0])
+        dp = [[0] * (col + 1) for _ in range(row + 1)]
+        res = 0
+        for i in range(1, row + 1):
+            for j in range(1, col + 1):
+                if matrix[i - 1][j - 1] == "1":
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+                    res = max(res, dp[i][j] ** 2)
+        return res

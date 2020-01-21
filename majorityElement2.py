4bdc0616cd060b3916cb1cb7e4e97185136e0742
Hcyand <897488242@qq.com>
2020-01-21 16:56:05 +0800

    求众数（投票法）

---
+++ majorityElement2.py
@@ -0,0 +1,40 @@
+"""
+给定一个大小为 n 的数组，找出其中所有出现超过 ⌊ n/3 ⌋ 次的元素。
+
+说明: 要求算法的时间复杂度为 O(n)，空间复杂度为 O(1)。
+
+示例 1:
+
+输入: [3,2,3]
+输出: [3]
+
+示例 2:
+
+输入: [1,1,1,3,3,2,2,2]
+输出: [1,2]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/majority-element-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+class Solution:
+    def majorityElement(self, nums: [int]) -> [int]:
+        candidate1 = candidate2 = None
+        cnt1 = cnt2 = 0
+        for num in nums:
+            if num == candidate1:
+                cnt1 += 1
+            elif num == candidate2:
+                cnt2 += 1
+            elif cnt1 == 0:
+                candidate1 = num
+                cnt1 = 1
+            elif cnt2 == 0:
+                candidate2 = num
+                cnt2 = 1
+            else:
+                cnt1 -= 1
+                cnt2 -= 1
+        return [n for n in (candidate1, candidate2) if nums.count(n) > len(nums) // 3]

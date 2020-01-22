c09459e3ed4b02659ce46d045937b9f5104e984e
Hcyand <897488242@qq.com>
2020-01-22 14:40:11 +0800

    滑动窗口最大值（动态分析）

---
+++ maxSlidingWindow.py
@@ -0,0 +1,52 @@
+"""
+给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+
+返回滑动窗口中的最大值。
+示例:
+输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+输出: [3,3,5,5,6,7]
+解释:
+  滑动窗口的位置                最大值
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+提示：
+你可以假设 k 总是有效的，在输入数组不为空的情况下，1 ≤ k ≤ 输入数组的大小。
+进阶：
+你能在线性时间复杂度内解决此题吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sliding-window-maximum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: [int], k: int) -> [int]:
+        n = len(nums)
+        if n * k == 0:
+            return []
+        if k == 1:
+            return nums
+        left = [0] * n
+        left[0] = nums[0]
+        right = [0] * n
+        right[n - 1] = nums[n - 1]
+        for i in range(1, n):
+            if i % k == 0:
+                left[i] = nums[i]
+            else:
+                left[i] = max(left[i - 1], nums[i])
+            j = n - i - 1
+            if (j + 1) % k == 0:
+                right[i] = nums[i]
+            else:
+                right[i] = max(right[j + 1], nums[j])
+        output = []
+        for i in range(n - k + 1):
+            output.append(max(left[i + k - 1], right[i]))
+        return output

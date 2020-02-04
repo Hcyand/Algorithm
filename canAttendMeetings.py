18c2f4abcdc9c951ad210fbea754531475fa6760
Hcyand <897488242@qq.com>
2020-02-04 17:18:24 +0800

    会议室问题 简单排序

--- canAttendMeetings.py
+++ canAttendMeetings.py
@@ -17,7 +17,7 @@
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
 
-
+# 排序解决
 class Solution:
     def canAttendMeetings(self, intervals: [[int]]) -> bool:
         intv = sorted(intervals, key=lambda x: x[0])
